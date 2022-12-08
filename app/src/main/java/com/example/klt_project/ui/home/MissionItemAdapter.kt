b492b6e7fde7c69aaa86d95f8f9c0d4538325b3a
GbrlBln <gabriel.balan209@gmail.com>
2022-12-08 22:46:50 +0100

    made mission layout using recycler view. this is just a sketch, it can be modified later.

---
+++ app/src/main/java/com/example/klt_project/ui/home/MissionItemAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.klt_project.ui.home
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.klt_project.R
+
+class MissionItemAdapter(
+    private val context: HomeFragment,
+    private val dataset: List<Missions>
+) : RecyclerView.Adapter<MissionItemAdapter.MissionItemViewHolder>(){
+
+    class MissionItemViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val missionName: TextView = view.findViewById(R.id.mission)
+        val from: TextView = view.findViewById(R.id.from)
+        val to: TextView = view.findViewById(R.id.to)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MissionItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_missions, parent, false)
+        return MissionItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: MissionItemViewHolder, position: Int) {
+        val item = dataset[position]
+        holder.missionName.text = context.resources.getString(item.stringMissionResourceId)
+        holder.from.text = context.resources.getString(item.stringFromResourceId)
+        holder.to.text = context.resources.getString(item.stringToResourceId)
+
+    }
+
+    override fun getItemCount() = dataset.size
+}
---
+++ app/src/main/java/com/example/klt_project/ui/home/Missions.kt
@@ -0,0 +1,10 @@
+package com.example.klt_project.ui.home
+
+import androidx.annotation.StringRes
+
+data class Missions(
+    @StringRes val stringMissionResourceId: Int,
+    @StringRes val stringFromResourceId: Int,
+    @StringRes val stringToResourceId: Int
+    ) {
+}
