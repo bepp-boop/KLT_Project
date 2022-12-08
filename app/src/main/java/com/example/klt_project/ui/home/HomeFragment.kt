91d72e97a51c6f6a17a0a325a55a7be12f91b96c
GbrlBln <gabriel.balan209@gmail.com>
2022-12-08 22:21:13 +0100

    changed the name of fragments and icon

--- app/src/main/java/com/example/klt_project/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/klt_project/ui/home/HomeFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.klt_project.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,17 +20,17 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val missionDataset = MissionDatasource().loadMissions()
+        val recyclerView = binding.recycler
+        recyclerView.adapter = MissionItemAdapter(this, missionDataset)
+        recyclerView.setHasFixedSize(true)
     }
 
     override fun onDestroyView() {
