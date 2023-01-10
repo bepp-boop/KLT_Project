ad2a20ba92cfb2c574c8674e0e3f9b809c3eb7db
Quang M Nguyen <quangteoqs15@gmail.com>
2023-01-10 21:08:30 +0100

    Pushed the mission that able to get the data and edit. Pushed to a static mission 10

--- app/src/main/java/com/example/klt_project/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/klt_project/ui/slideshow/SlideshowFragment.kt
@@ -6,17 +6,22 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.klt_project.data.Report
 import com.example.klt_project.databinding.FragmentSlideshowBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_slideshow.*
 import org.w3c.dom.Text
 
 class SlideshowFragment : Fragment() {
 
     private var _binding: FragmentSlideshowBinding? = null
-
+    private val database = Firebase.database("https://klt-prototype-default-rtdb.europe-west1.firebasedatabase.app/")
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
@@ -38,10 +43,10 @@ class SlideshowFragment : Fragment() {
         var washTime = ""
         var noteS = ""
 
-        val loadingText: TextView = binding.loadingTime
-        val drivingText: TextView = binding.drivingTime
-        val unloadingText: TextView = binding.unloadingTime
-        val washingText: TextView = binding.washingTime
+        val loadingText: EditText = binding.loadingTime
+        val drivingText: EditText = binding.drivingTime
+        val unloadingText: EditText = binding.unloadingTime
+        val washingText: EditText = binding.washingTime
 
         slideshowViewModel.text.observe(viewLifecycleOwner) {
             loadTime = "${it["hour"]?.get(0).toString()}:${it["minutes"]?.get(0).toString()}:${it["seconds"]?.get(0).toString()}"
@@ -49,20 +54,33 @@ class SlideshowFragment : Fragment() {
             unloadTime = "${it["hour"]?.get(2).toString()}:${it["minutes"]?.get(2).toString()}:${it["seconds"]?.get(2).toString()}"
             washTime = "${it["hour"]?.get(3).toString()}:${it["minutes"]?.get(3).toString()}:${it["seconds"]?.get(3).toString()}"
 
-            loadingText.text = loadTime
-            drivingText.text = drvTime
-            unloadingText.text = unloadTime
-            washingText.text = washTime
+            loadingText.setText(loadTime)
+            drivingText.setText(drvTime)
+            unloadingText.setText(unloadTime)
+            washingText.setText(washTime)
         }
 
-        val showNote:TextView = binding.noteEdit
+        val showNote:EditText = binding.noteEdit
         slideshowViewModel.note.observe(viewLifecycleOwner){
             noteS = it
-            showNote.text = noteS
+            showNote.setText(noteS)
         }
-        val report = Report(loadTime, drvTime, unloadTime, washTime, noteS)
-        Log.d("report", report.note)
 
+        binding.sendButton.setOnClickListener {
+            val report = HashMap<String,Any>()
+            loadingText.setText(loadingText.text.toString())
+            drivingText.setText(drivingText.text.toString())
+            unloadingText.setText(unloadingText.text.toString())
+            washingText.setText(washingText.text.toString())
+            showNote.setText(showNote.text.toString())
+            report.put("loading time",loadingText.text.toString())
+            report.put("driving time",drivingText.text.toString())
+            report.put("unLoading time",unloadingText.text.toString())
+            report.put("wash time",washingText.text.toString())
+            report.put("note",showNote.text.toString())
+            database.getReference("Mission").child(10.toString()).updateChildren(report)
+            Log.d("Upload correct","yea")
+        }
         return root
     }
 
